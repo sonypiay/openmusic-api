4f66591581629268b2a5d79eded70a18ea3f0807
sonypiay <pyscho30@gmail.com>
2025-09-03 00:30:15 +0700

    remove listener function

--- src/application/Configuration.js
+++ src/application/Configuration.js
@@ -5,13 +5,6 @@ export default {
         host: process.env.HOST ?? 'localhost',
         port: process.env.PORT ?? 5000,
     },
-    smtp: {
-        host: process.env.SMTP_HOST,
-        port: process.env.SMTP_PORT,
-        user: process.env.SMTP_USER,
-        password: process.env.SMTP_PASSWORD,
-        from: process.env.SMTP_FROM,
-    },
     rabbitmq: {
         url: process.env.RABBITMQ_URL,
     },
